a3e47d6c61f99833aa2d0b3e81b07c1e5963c895
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-12-02 17:14:05 -0800

    Added half comments

--- client/client1/client.py
+++ client/client1/client.py
@@ -1,7 +1,17 @@
 import requests
+
+# Set up url for send request to
+# In this case the url is of proxy server,
+# That acts a load balancer to 
+# Distribute jobs to worker nodes
 url = 'http://localhost:8081'
+
+# opening a test file with python code inside 
+# Ready to be executed
 
 with open('test.py', 'rb') as f:
     output = requests.post(url, data=f)
+
+#print the output recieved from the server
 
 print(output.text)
--- server/server.py
+++ server/server.py
@@ -4,26 +4,52 @@ from flask import request
 import flask
 import requests
 import time
-app, self_host, self_port, nodes = flask.Flask(__name__), "0.0.0.0", 8081, [
-    'http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+# app, self_host, self_port, nodes = flask.Flask(__name__), "0.0.0.0", 8081, [
+#     'http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+
+# Setting up flask server
+# Acting as a proxy between the worker nodes and client
+# The server will recieve files from the clients
+
+app = flask.Flask(__name__)
+self_host = "0.0.0.0"
+self_port = 8081
+
+# Instantiating the worker nodes in a array.
+# Two nodes running on 8000, 8001 ports 
+
+nodes = ['http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+
 requestQueue = []
 
+#File Id to create unique files based on requests
 fileId = 1000
 
-
+# RequestWorker function sends the file data to worker nodes
+# Parameters: request object and hosted worker URL
 def requestWorker(req, workerUrl):
     global fileId
     fileId += 1
     filename = 'request' + str(fileId) + '.py'
+
+    # Saving the file in local dir of server
+
     FileStorage(req.stream).save(
         os.path.join('./', filename))
     with open(filename, 'rb') as f:
+
+        # Send the POST request to workerUrl
+        # recieve the output ones done.
         output = requests.post(workerUrl + str(fileId), data=f).text
     # print(output)
+
+    #remove the temporary created file.
     os.remove(filename)
+    # Return back to calling route function
     return output, 200
 
-
+# Route function: recieves the requests from client
+# Function servers as main component of the architecture
 @app.errorhandler(404)
 def route_page(err):
     global nodes
