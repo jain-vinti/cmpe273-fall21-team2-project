81223c7530b9066cb3ff1d4d5650f82ff9812e95
rugvedmanoorkar <manoorkar.rugved@gmail.com>
2021-12-02 17:33:21 -0800

    Rugved/comments (#6)
    * Added half comments
    
    * added comments all

--- server/server.py
+++ server/server.py
@@ -4,37 +4,71 @@ from flask import request
 import flask
 import requests
 import time
-app, self_host, self_port, nodes = flask.Flask(__name__), "0.0.0.0", 8081, [
-    'http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+# app, self_host, self_port, nodes = flask.Flask(__name__), "0.0.0.0", 8081, [
+#     'http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+
+# Setting up flask server
+# Acting as a proxy between the worker nodes and client
+# The server will recieve files from the clients
+
+app = flask.Flask(__name__)
+self_host = "0.0.0.0"
+self_port = 8081
+
+# Instantiating the worker nodes in a array.
+# Two nodes running on 8000, 8001 ports 
+
+nodes = ['http://127.0.0.1:8000/', 'http://127.0.0.1:8001/']
+
 requestQueue = []
 
+#File Id to create unique files based on requests
 fileId = 1000
 
-
+# RequestWorker function sends the file data to worker nodes
+# Parameters: request object and hosted worker URL
 def requestWorker(req, workerUrl):
     global fileId
     fileId += 1
     filename = 'request' + str(fileId) + '.py'
+
+    # Saving the file in local dir of server
+
     FileStorage(req.stream).save(
         os.path.join('./', filename))
     with open(filename, 'rb') as f:
+
+        # Send the POST request to workerUrl
+        # recieve the output ones done.
         output = requests.post(workerUrl + str(fileId), data=f).text
     # print(output)
+
+    #remove the temporary created file.
     os.remove(filename)
+    # Return back to calling route function
     return output, 200
 
-
+# Route function: recieves the requests from client
+# Function servers as main component of the architecture
 @app.errorhandler(404)
 def route_page(err):
     global nodes
+    
     global requestQueue
+    # Append each request to request Queue
     requestQueue.append(request)
     print(nodes, requestQueue)
+
+    #If no nodes are available wait for workers to finish jobs
     while nodes == []:
         continue
+    # if available pop a worker node and request in Request Queue
     currNode = nodes.pop(0)
     currReq = requestQueue.pop(0)
+
+    # Call and request Worker node to send and run the file 
     output = requestWorker(currReq, currNode)
+    # Put the node back into nodes
     nodes.append(currNode)
     return output
 
